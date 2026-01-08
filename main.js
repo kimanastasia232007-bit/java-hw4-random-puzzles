6f2fe76f493313ce53f1cffe7a6edb7d51d40a96
st <st@gmail.com>
2026-01-08 11:08:05 +0500

    save

--- main.js
+++ main.js
@@ -3,20 +3,20 @@ let quantity = +prompt('Введите количество примеров')
 function random(min,max) {
     return Math.floor((Math.random() * (max + 1 - min) + min) )
 }
-random(1,4)
+for(let i = 1; i <= quantity; i++) {
 let num1 = random(1, 10)
 let num2 = random(1, 10)
-for(let i = 1; i <= quantity; i++) {
-if(random(1,4) == 1) {
+let randoom = random(1,4)
+if( randoom == 1) {
 let answer1 = +prompt(`${num1} + ${num2}= `)
 if(answer1 == num1 + num2) {
     console.log(`Ваш ответ верный - ${answer1}`);
 }
 else {
-    console.log(`Ваш ответ верный - ${answer1}. Правильный ответ -  ${num1 + num2}`);
+    console.log(`Ваш ответ неверный - ${answer1}. Правильный ответ -  ${num1 + num2}`);
 }
 }
-if(random(1,4) == 2) {
+else if(randoom == 2) {
 let answer2 = +prompt(`${num1} - ${num2}= `)
 if(answer2 == num1 - num2) {
     console.log(`Ваш ответ верный - ${answer2}`);
@@ -25,8 +25,8 @@ else {
     console.log(`Ваш ответ верный - ${answer2}. Правильный ответ -  ${num1 - num2}`);
 }
 }
-if(random(1,4) == 3) {
-let answer3 = +prompt(`${num1} X ${num2}= `)
+else if(randoom == 3) {
+let answer3 = +prompt(`${num1} х ${num2}= `)
 if(answer3 == num1 * num2) {
     console.log(`Ваш ответ верный - ${answer3}`);
 }
@@ -34,7 +34,7 @@ else {
     console.log(`Ваш ответ верный - ${answer3}. Правильный ответ -  ${num1 * num2}`);
 }
 }
-if(random(1,4) == 4) {
+else if(randoom == 4) {
 let answer1 = +prompt(`${num1} ÷ ${num2}= `)
 if(answer1 == num1 / num2) {
     console.log(`Ваш ответ верный - ${answer1}`);
